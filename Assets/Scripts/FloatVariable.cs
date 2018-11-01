110c9848e52e36e2397cfa7a9a34100091764802
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-11-01 12:00:45 -0500

    added varaible class and floatvariable scriptables

---
+++ Assets/Scripts/FloatVariable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Variables/Float")]
+public class FloatVariable : Variable
+{
+    public int _value;
+    public int _MaxValue;
+    public override object Value
+    {
+        get
+        {
+            return _value;
+        }
+    }
+
+    public override object MaxValue
+    {
+        get
+        {
+            return _MaxValue;
+        }
+    }
+}
