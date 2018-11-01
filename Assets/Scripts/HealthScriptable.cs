74189a5d63a3b7493a9f820bfe4779ae97262d9a
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-11-01 12:01:10 -0500

    added healthscriptable that inherits from float variable

---
+++ Assets/Scripts/HealthScriptable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "_IntVariable")]
+public class HealthScriptable : FloatVariable, IDamageable
+{
+    [SerializeField] public float _mMaxValue;
+    [SerializeField] public float _mValue;
+
+    public float Value
+    {
+        get { return _mValue; }
+        set
+        {
+            _mValue = value;
+        }
+    }
+    public float MaxValue
+    {
+        get { return _mMaxValue; }
+        set
+        {
+            _mMaxValue = value;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        Value -= amount;
+    }
+}
