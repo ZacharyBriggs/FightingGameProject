cde9117acd955c8a1cd1765cb35a7e4b810d9883
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-11-01 12:03:31 -0500

    hadouken can now move to the right and can collide

---
+++ Assets/Scripts/HadoukenBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HadoukenBehaviour : MonoBehaviour
+{
+    public Vector3 Force;
+    public float Mass;
+    public float LifeTime;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var vel = Force / Mass;
+        this.transform.position += vel * Time.deltaTime;
+        LifeTime -= Time.deltaTime;
+        if (LifeTime <= 0)
+            Destroy(this.gameObject);
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        var other = col.GetComponent<IDamageable>();
+        other.TakeDamage(10);
+        Destroy(this.gameObject);
+    }
+}
