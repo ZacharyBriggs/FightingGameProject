dec817239d454d156ce00ffcacea2bd33908c21b
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-11-02 17:12:59 -0500

    behaviour for any hitboxes

---
+++ Assets/Scripts/HitBoxBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBoxBehaviour : MonoBehaviour, IDamager
+{
+    public float DamageAmount;
+    public void DealDamage(IDamageable other, float amount)
+    {
+        other.TakeDamage(amount);
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        var damagable = col.GetComponent<IDamageable>();
+        DealDamage(damagable, DamageAmount);
+    }
+}
