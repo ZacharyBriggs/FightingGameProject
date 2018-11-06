4860e638d9aeaa877805060b289af4456b62fe17
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-11-06 16:05:20 -0600

    added a scriptable for super attack meter

---
+++ Assets/Scripts/MeterScriptable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class MeterScriptable : FloatVariable
+{
+    [SerializeField] public float _mMaxValue;
+    [SerializeField] public float _mValue;
+
+    public float Value
+    {
+        get { return _mValue; }
+        set
+        {
+            _mValue = value;
+        }
+    }
+    public float MaxValue
+    {
+        get { return _mMaxValue; }
+        set
+        {
+            _mMaxValue = value;
+        }
+    }
+
+    public void AddToValue(float amount)
+    {
+        Value += amount;
+    }
+}
