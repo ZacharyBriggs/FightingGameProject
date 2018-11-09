2bd847ec0a545387c44a6743335628845cd9bafa
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-11-09 16:51:15 -0600

    added triggers for specials and added ultra attack with prefabs

--- Assets/Scripts/RyuBehaviour.cs
+++ Assets/Scripts/RyuBehaviour.cs
@@ -7,8 +7,11 @@ public class RyuBehaviour : FighterBehaviour
     public InputScriptable SpAttack1;
     public InputScriptable SpAttack2;
     public InputScriptable SpAttack3;
+    public InputScriptable UAttack1;
     public Transform HadoukenSpawnPos;
+    public Transform ShinkuuHadoukenSpawnPos;
     public GameObject HadoukenPrefab;
+    public GameObject SHadoukenPrefab;
 	// Use this for initialization
 	void Start ()
     {
@@ -29,61 +32,69 @@ public class RyuBehaviour : FighterBehaviour
         Instantiate(HadoukenPrefab, HadoukenSpawnPos);
     }
 
-    public override void CheckInput()
+    public void DoUltraOne()
+    {
+        Instantiate(SHadoukenPrefab, ShinkuuHadoukenSpawnPos);
+    }
+    public override bool CheckInput()
     {
         if (InputList.Count > 4)
         {
+            int a = 0;
+            int b = 0;
             int c = 0;
-            for (int i = SpAttack3.MoveInput.Count; i > 0; i--)
+            int d = 0;
+            for (int i = UAttack1.MoveInput.Count; i > 0; i--)
             {
-                if (InputList[InputList.Count - i] != SpAttack3.MoveInput[c])
-                    break;
-                else
+                if (InputList[InputList.Count - i] == UAttack1.MoveInput[d])
+                    d++;
+
+                if (d == UAttack1.MoveInput.Count)
+                {
+                    _animator.SetTrigger("ShinkuuHadoukenTrigger");
+                    Debug.Log("U1");
+                    InputList = new List<string> { "None" };
+                    return true;
+                }
+            }
+
+            for (int i = SpAttack1.MoveInput.Count; i > 0; i--)
+            {
+                
+
+                if (InputList[InputList.Count - i] == SpAttack1.MoveInput[a])
+                    a++;
+
+                if (a == SpAttack1.MoveInput.Count)
+                {
+                    _animator.SetTrigger("HadoukenTrigger");
+                    Debug.Log("Sp1");
+                    InputList = new List<string> { "None" };
+                    return true;
+                }
+                if (InputList[InputList.Count - i] == SpAttack2.MoveInput[b])
+                    b++;
+
+                if (b == SpAttack2.MoveInput.Count)
+                {
+                    _animator.SetTrigger("TatsumakiTrigger");
+                    Debug.Log("Sp2");
+                    InputList = new List<string> { "None" };
+                    return true;
+                }
+                if (InputList[InputList.Count - i] == SpAttack3.MoveInput[c])
                     c++;
 
                 if (c == SpAttack3.MoveInput.Count)
                 {
-                    _animator.SetInteger("State", 17);
-                    InputList.Add("Sp3");
+                    _animator.SetTrigger("ShoryukenTrigger");
                     Debug.Log("Sp3");
                     InputList = new List<string> { "None" };
+                    return true;
                 }
             }
             
-            int b = 0;
-            for (int i = SpAttack2.MoveInput.Count; i > 0; i--)
-            {
-                if (InputList[InputList.Count - i] != SpAttack2.MoveInput[b])
-                    break;
-                else
-                    b++;
-
-                if (b == 4)
-                {
-                    _animator.SetInteger("State", 16);
-                    InputList.Add("Sp2");
-                    Debug.Log("Sp2");
-                    InputList = new List<string> { "None" };
-                }
-            }
-
-            int a = 0;
-            for (int i = SpAttack1.MoveInput.Count; i > 0; i--)
-            {
-                if (InputList[InputList.Count - i] != SpAttack1.MoveInput[a])
-                    break;
-                else
-                    a++;
-
-                if (a == 4)
-                {
-                    _animator.SetInteger("State", 15);
-                    InputList.Add("Sp1");
-                    Debug.Log("Sp1");
-                    InputList = new List<string> { "None" };
-                }
-            }
-
         }
+        return false;
     }
 }
